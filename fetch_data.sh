445304debca30c8520e8f367749d118b3dfe21a4
kurzs6 <stephan.kurz@oeaw.ac.at>
2024-04-04 18:49:45 +0200

    path correction

--- fetch_data.sh
+++ fetch_data.sh
@@ -7,9 +7,9 @@ rm -rf data/meta && mkdir data/meta
 curl -LO https://github.com/KONDE-AT/maechtekongresse/archive/refs/heads/master.zip
 unzip master
 
-mv ./maechtekongresse-master/data/editions/ ./data
-mv ./maechtekongresse-master/data/indices/ ./data
-mv ./maechtekongresse-master/data/meta/ ./data
+mv ./maechtekongresse-master/data/editions ./data
+mv ./maechtekongresse-master/data/indices ./data
+mv ./maechtekongresse-master/data/meta ./data
 
 rm master.zip
 rm -rf ./maechtekongresse-master

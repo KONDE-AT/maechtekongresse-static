940abdfed3f509b9c630b29a7b09c003b963a456
csae8092 <peter.andorfer@oeaw.ac.at>
2024-04-04 08:44:25 +0200

    little refactoring

--- process.sh
+++ process.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+echo "add ids"
+add-attributes -g "./data/editions/*.xml" -b "https://id.acdh.oeaw.ac.at/maechtekongresse/editions"
+
+echo "denormalize indices"
+denormalize-indices -f "./data/editions/*.xml" -i "./data/indices/*.xml" -m ".//*[@ref]/@ref" -x ".//tei:title[@type='main']/text()"
+
+echo "remove notegrpoups from edition files"
+python rm_notegrp.py
 
 echo "building calendar"
 python make_calendar_data.py

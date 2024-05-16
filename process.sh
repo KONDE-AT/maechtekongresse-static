5a362e175848216db11ff50a77510e01e7cfed2d
Stephan Kurz <stephan.kurz@oeaw.ac.at>
2024-05-16 16:44:48 +0200

    handling entities in meta/*xml as well

--- process.sh
+++ process.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+echo "remove netvis stuff"
+rm "./data/meta/netviz-config.xml"
+
 echo "add ids"
 add-attributes -g "./data/editions/*.xml" -b "https://id.acdh.oeaw.ac.at/maechtekongresse/editions"
+add-attributes -g "./data/meta/*.xml" -b "https://id.acdh.oeaw.ac.at/maechtekongresse/meta"
 
 echo "denormalize indices"
-denormalize-indices -f "./data/editions/*.xml" -i "./data/indices/*.xml" -m ".//*[@ref]/@ref" -x ".//tei:title[@type='main']/text()"
+denormalize-indices -f "./data/*/*.xml" -i "./data/indices/*.xml" -m ".//*[@ref]/@ref" -x ".//tei:title[@type='main']/text()"
 
 echo "remove notegroups from edition files"
 python rm_notegrp.py
